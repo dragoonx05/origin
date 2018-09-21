964229f3e64770bdab5ffb692222057a09ce5568
Boon Tek <darkon.garon@gmail.com>
2018-09-21 15:36:56 +0800

    1. modified seeds.rb with fake info

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = {}
+user["password"] = '123456'
+
+ActiveRecord::Base.transaction do
+    10.times do 
+        user['first_name'] = Faker::Name.first_name
+        user['last_name'] = Faker::Name.last_name
+        user['email'] = Faker::Internet.email
+        
+        User.create(user)
+    end
+end
+
+listing = {}
+uids = []
+User.all.each { |u|  uids << u.id}
+
+ActiveRecord::Base.transaction do
+    30.times do 
+        listing['name'] = Faker::App.name
+        listing['description'] = Faker::Hipster.sentence
+        listing['address'] = Faker::Address.full_address
+        listing['price'] = rand(50..300)
+        listing['rating'] = rand(0..5)
+        listing['bed_room_type'] = ["Studio", "House", "Entire Floor", "Condominium", "Villa", "Townhouse", "Castle", "Treehouse", "Igloo", "Yurt", "Cave", "Chalet", "Hut", "Tent", "Others"].sample
+        listing['bed_room_num'] = rand(1..3)
+        listing['bed_type'] = ["Single", "Queen Size", "King Size", "Bunk Bed", "No Bed"].sample
+        listing['bed_num'] = rand(1..3)
+        listing['bathroom_type'] = ["Private", "Shared"].sample
+        listing['bathroom_num'] = rand(1..2)
+
+        listing['user_id'] = uids.sample
+    end
+end
